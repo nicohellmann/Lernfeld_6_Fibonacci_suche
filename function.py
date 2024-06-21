25ae438107cfcdfe4330b75236b075c8890bfa78
unknown <hellmann1.nico@gmx.de>
2024-06-21 12:22:22 +0200

    initial commit

---
+++ function.py
@@ -0,0 +1,18 @@
+class function:
+    # parameter list are ordered by exponents x_parameters[0] has exponent 0 
+    def __init__(self,polynom = True,x_parameters:list = [],y_parameters:list=[]) -> None:
+        self.x_parameters = x_parameters
+        self.y_parameters = y_parameters
+
+    def solve(self,x,y=None):
+        re=0
+        if(y==None):
+            for i in range(0,len(self.x_parameters)):
+                re += self.x_parameters[i]**i
+            return re
+        else:
+            for i in range(0,len(self.x_parameters)):
+                re += self.x_parameters[i]**i
+            for i in range(0,len(self.y_parameters)):
+                re += self.y_parameters[i]**i
+            return re
